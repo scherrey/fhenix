00b8c33a54b38f9ab5fef0124800199603ce8c6b
Benjamin Scherrey <scherrey@biggestlab.io>
2023-07-21 16:04:32 +0200

    If we get lucky announce & end early.

--- test/share_price.py
+++ test/share_price.py
@@ -12,7 +12,11 @@ def share_price(_assets : int, _shares : int) -> int :
 
 	for _i in range(Iterations):
 		mid = (low + high) // 2
-		if mid * _shares < _assets:
+		new_guess = mid * _shares
+		if new_guess == _assets: 
+			print("Converged @ %s!" % (_i))
+			break
+		if new_guess < _assets:
 			low = mid
 		else:
 			high = mid
