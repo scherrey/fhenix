8752e838ddc2d8953dbf416fed4ee2d63283bb7b
Benjamin Scherrey <scherrey@biggestlab.io>
2023-07-21 14:58:32 +0200

    Do a proper % rounded to 5 decimals.

--- test/share_price.py
+++ test/share_price.py
@@ -22,7 +22,7 @@ def share_price(_assets : int, _shares : int) -> int :
 convergent_price = share_price(Assets, Shares)
 actual_price = Assets // Shares
 diff = abs(convergent_price - actual_price)
-percentage = diff / actual_price
+percentage = round((diff / actual_price) * 100, 5)
 print("%s assets with %s shares is %s per share." % (Assets, Shares, convergent_price))
 print("Actual value should be %s." % (actual_price))
 print("Difference is %s%%." % (percentage))
